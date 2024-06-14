81ca6dda72399afcc41bf7fd06e9ba65e77af497
Alanv3514 <alanv3514@gmail.com>
2024-06-14 20:09:52 -0300

    agrego database.php

--- config/database.php
+++ config/database.php
@@ -1,4 +1,18 @@
 <?php
 
+return [
+    'database' => [
+        'driver'    => 'mysql',
+        'host'      => getenv('DATABASE_HOST'),
+        'name'  => getenv('DATABASE_NAME'),
+        'username'  => getenv('DATABASE_USERNAME'),
+        'password'  => getenv('DATABASE_PASSWORD'),
+        'port'=> getenv('DATABASE_PORT'),
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ],
+];
+
 
 ?>
