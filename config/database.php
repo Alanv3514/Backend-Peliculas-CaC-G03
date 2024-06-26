3fe3fc5a792855712caa133a4b63a64617a358a9
rik <ingeniero_rik@hotmail.com>
2024-06-26 20:00:55 -0300

    database.php modif

--- config/database.php
+++ config/database.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 <?php
@@ -13,19 +12,4 @@ $db = [
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '', //Cambiar al nombre de tu base de datos
 ];
-?>
-=======
-<?php
-    $db = [
-        'driver'    => 'mysql',
-        'host'      => getenv('DATABASE_HOST'),
-        'db'  => getenv('DATABASE_NAME'),
-        'username'  => getenv('DATABASE_USERNAME'),
-        'password'  => getenv('DATABASE_PASSWORD'),
-        'port'=> getenv('DATABASE_PORT'),
-        'charset'   => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'prefix'    => '', //Cambiar al nombre de tu base de datos
-    ];
->>>>>>> 9eadbb6777b1cb1df795925ceeb259e9e1809fa2
 ?>
